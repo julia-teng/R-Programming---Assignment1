066fe3fc54849897cd334bf8cc0e5ee0e2be83d0
julia-teng <julia.teng83@gmail.com>
2018-12-06 17:26:14 -0500

    getting directory

---
+++ pollutantmean.R
@@ -0,0 +1,6 @@
+pollutantmean <- function(directory,pollutant,id){
+  
+  ## directory is a character vector of length 1 
+  ## indicating the location og the CSV files
+  directory <- paste(getwd(),"/",directory,"/",sep="")
+}
