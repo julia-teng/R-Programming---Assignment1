6b61b63a8118221b579fe5fb8fbfac848bfa4928
julia-teng <julia.teng83@gmail.com>
2018-12-06 17:58:07 -0500

    Part 1 done

--- pollutantmean.R
+++ pollutantmean.R
@@ -2,5 +2,25 @@ pollutantmean <- function(directory,pollutant,id){
   
   ## directory is a character vector of length 1 
   ## indicating the location og the CSV files
+  
+  ## pollutant is a character vector of length 1 indicating
+  ## the name of the pollutant for which we will calculate
+  ## the mean; either sulfate or nitrate
+  
+  ## id is an integer vector indicating the monitor id 
+  ## numbers to be used
   directory <- paste(getwd(),"/",directory,"/",sep="")
+  directory
+
+  filelist <-list.files(directory,pattern=".csv",full.names = TRUE)
+  ## Empty for now
+  data <- NA
+
+  for(i in id ){
+    file_data <- read.csv(filelist[i])
+
+    data <- rbind(data,file_data)
+  }
+  mean(data[[pollutant]],na.rm = TRUE)
+  
 }
